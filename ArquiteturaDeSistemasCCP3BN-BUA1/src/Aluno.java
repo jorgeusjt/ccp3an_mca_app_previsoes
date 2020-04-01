0aa450d9ed6cdd069aab6aaf0bf107d0ae1978db
Jorge Sampaio <jorgelimass@hotmail.com>
2020-04-01 01:18:51 -0300

    Adicionando Gets and Sets

--- ArquiteturaDeSistemasCCP3BN-BUA1/src/Aluno.java
+++ ArquiteturaDeSistemasCCP3BN-BUA1/src/Aluno.java
@@ -1,6 +1,21 @@
-public class Aluno{
-   private String nome;
-   private int id;
-   
+public class Aluno {
+	private String nome;
+	private int id;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
 }
-   
